36d4c951e35941d5b28c7dc115a8e1a14e71700a
atheius <atheiusdev@gmail.com>
2022-01-15 16:29:47 +0000

    Add separate axios instance.

--- src/plugins/putio.js
+++ src/plugins/putio.js
@@ -3,40 +3,52 @@ const axios = require("axios");
 const qs = require("querystring");
 
 async function putio(fastify, opts) {
-  axios.defaults.baseURL = "https://api.put.io/v2";
-  axios.defaults.headers.common["Authorization"] = `Bearer ${opts.accessToken}`;
-  axios.interceptors.response.use((res) => res.data);
+  const axiosInstance = axios.create({
+    baseURL: "https://api.put.io/v2",
+    headers: {
+      Authorization: `Bearer ${opts.accessToken}`,
+    },
+  });
+
+  axiosInstance.interceptors.response.use((res) => res.data);
 
   async function getFiles() {
-    return axios.get("/files/list");
+    return axiosInstance.get("/files/list");
   }
 
   async function createZip(fileId) {
-    return axios.post("/zips/create", qs.stringify({ file_ids: fileId }), {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    });
+    return axiosInstance.post(
+      "/zips/create",
+      qs.stringify({ file_ids: fileId }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
   }
 
   async function checkZipStatus(zipId) {
-    return axios.get(`/zips/${zipId}`);
+    return axiosInstance.get(`/zips/${zipId}`);
   }
 
   async function getDownloadStream(url) {
-    return axios({
+    return axiosInstance.get({
       url,
-      method: "GET",
       responseType: "stream",
     });
   }
 
   async function deleteFile(fileId) {
-    return axios.post("/files/delete", qs.stringify({ file_ids: fileId }), {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    });
+    return axiosInstance.post(
+      "/files/delete",
+      qs.stringify({ file_ids: fileId }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
   }
 
   fastify.decorate("putio", {
