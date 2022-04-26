bca594dd4383236ee09e7ccf4b3188172bbd531c
atheius <atheiusdev@gmail.com>
2022-04-26 11:50:17 +0100

    Improved zip retry logic.

--- src/plugins/processor.js
+++ src/plugins/processor.js
@@ -3,7 +3,9 @@ const fs = require("fs");
 const fp = require("fastify-plugin");
 const stream = require("stream");
 const unzip = require("unzip-stream");
+const retry = require("retry");
 const { promisify } = require("util");
+
 const pipeline = promisify(stream.pipeline);
 const sleep = promisify(setTimeout);
 
@@ -20,7 +22,7 @@ async function processorPlugin(fastify, opts) {
         deleteFile,
       } = fastify.putio;
 
-      fastify.log.info({ fileId }, `Checking file info [${fileId}]`);
+      fastify.log.info({ fileId }, `checking file info [${fileId}]`);
 
       const {
         file: { name: fileName },
@@ -30,25 +32,37 @@ async function processorPlugin(fastify, opts) {
 
       const { zip_id: zipId } = await createZip(fileId);
 
-      let url = null;
-      let retryCount = 0;
+      const operation = retry.operation({
+        retries: 10,
+        factor: 4,
+        minTimeout: 1 * 3000,
+        maxTimeout: 120 * 1000,
+        randomize: true,
+      });
 
-      await sleep(1500);
+      const url = await new Promise(async (resolve, reject) => {
+        await sleep(3000);
 
-      while (!url && retryCount <= 3) {
-        fastify.log.info({ zipId }, "checking zip status");
-        const res = await checkZipStatus(zipId);
-        url = res.url;
-        if (!url) {
-          fastify.log.warn({ zipId }, `zip file not ready [${fileName}]`);
-          retryCount += 1;
-          await sleep(3000);
-        }
-      }
+        operation.attempt(async function (currentAttempt) {
+          fastify.log.info(
+            { zipId },
+            `checking zip status attempt ${currentAttempt}`
+          );
 
-      if (!url) {
-        throw new Error({ zipId }, `failed to get zip file [${fileName}]`);
-      }
+          const res = await checkZipStatus(zipId);
+
+          if (operation.retry(!res.url)) {
+            fastify.log.warn({ zipId }, `zip file not ready [${fileName}]`);
+            return;
+          }
+
+          if (res.url) {
+            return resolve(res.url);
+          }
+
+          return reject("failed to create zip file in time");
+        });
+      });
 
       fastify.log.info({ fileId, zipId }, `starting download [${fileName}]`);
 
